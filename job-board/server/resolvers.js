426faad08e43584bb0711176ef69866cd8d99004
lautaronahuelc <lautaronahuel.coria@gmail.com>
2024-09-25 22:05:42 -0300

    Updated 2024_09_25

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,11 +1,35 @@
-import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
+import { GraphQLError } from 'graphql';
+import { createJob, deleteJob, getJob, getJobs, getJobsByCompany, updateJob } from "./db/jobs.js";
 import { getCompany } from './db/companies.js'
 
 export const resolvers = {
   Query: {
-    company: (_root, { id }) => getCompany(id),
-    job: (_root, { id }) => getJob(id), 
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if (!company) {
+        throw notFoundError('No Company found with id' + id);
+      }
+      return company;
+    },
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if (!job) {
+        throw notFoundError('No Job found with id' + id);
+      }
+      return job;
+    }, 
     jobs: () => getJobs(),
+  },
+
+  Mutation: {
+    createJob: (_root, { input: { title, description } }) => {
+      const companyId = 'FjcJCHJALA4i'; // TODO set based on user
+      return createJob({ companyId, title, description });
+    },
+    deleteJob: (_root, { id }) => deleteJob(id),
+    updateJob: (_root, { input: { id, title, description } }) => {
+      return updateJob({ id, title, description });
+    },
   },
 
   Company: {
@@ -17,6 +41,12 @@ export const resolvers = {
     date: (job) => toIsoDate(job.createdAt),
   }
 };
+
+function notFoundError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: 'NOT_FOUND' },
+  });
+}
 
 function toIsoDate(value) {
   return value.slice(0, 'yyyy-mm-dd'.length);
