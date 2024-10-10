a3bada0bd94de60a8b9fb86190aacd1f548a25a9
lautaronahuelc <lautaronahuel.coria@gmail.com>
2024-10-09 22:29:53 -0300

    Updated 2024_10_09

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -22,13 +22,31 @@ export const resolvers = {
   },
 
   Mutation: {
-    createJob: (_root, { input: { title, description } }) => {
-      const companyId = 'FjcJCHJALA4i'; // TODO set based on user
-      return createJob({ companyId, title, description });
+    createJob: (_root, { input: { title, description } }, { user }) => {
+      if (!user) {
+        throw unauthorizedError('Missing authentication');
+      }
+      return createJob({ companyId: user.companyId, title, description });
     },
-    deleteJob: (_root, { id }) => deleteJob(id),
-    updateJob: (_root, { input: { id, title, description } }) => {
-      return updateJob({ id, title, description });
+    deleteJob: async (_root, { id }, { user }) => {
+      if (!user) {
+        throw unauthorizedError('Missing authentication');
+      }
+      const job = await deleteJob(id, user.companyId);
+      if (!job) {
+        throw notFoundError('No Job found with id' + id);
+      }
+      return job;
+    },
+    updateJob: async (_root, { input: { id, title, description } }, { user }) => {
+      if (!user) {
+        throw unauthorizedError('Missing authentication');
+      }
+      const job = await updateJob({ id, companyId: user.companyId, title, description });
+      if (!job) {
+        throw notFoundError('No Job found with id' + id);
+      }
+      return job;
     },
   },
 
@@ -45,6 +63,12 @@ export const resolvers = {
 function notFoundError(message) {
   return new GraphQLError(message, {
     extensions: { code: 'NOT_FOUND' },
+  });
+}
+
+function unauthorizedError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: 'UNAUTHORIZED' },
   });
 }
 
